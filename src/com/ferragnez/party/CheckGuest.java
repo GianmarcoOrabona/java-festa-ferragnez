fa38be8029e7b497b2975a60d4179768d9e1d21c
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2023-11-30 13:33:13 +0100

    add guests array

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,7 +1,14 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Creo l'array di invitati
+        String[] invitati  = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Paradis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
 
     }
 }
